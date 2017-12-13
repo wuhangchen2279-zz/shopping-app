752359ddd5c13f69f61ab993ca4c163ca8172d4d
Daniel Wu <daniel@ofsystems.com>
2017-12-13 16:50:03 +1100

    pages for frontend

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('testRec', 'test desc', 'http://www.seriouseats.com/images/2017/02/20170228-pressure-cooker-recipes-roundup-collage.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
